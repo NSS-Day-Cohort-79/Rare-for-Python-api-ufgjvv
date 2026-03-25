b5e777978ae691a1daa23130af43d0110a5092c5
Jordan Haddock <haddockjordan40@gmail.com>
2026-03-25 11:10:09 -0500

    initial commit

--- views/posts.py
+++ views/posts.py
@@ -2,39 +2,50 @@ import sqlite3
 import json
 from datetime import datetime
 
+
 def post_post(post):
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         INSERT INTO Posts ('user_id', 'category_id', 'title', 'publication_date', 'image_url', 'content', 'approved')
                           VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (
-            post["user_id"],
-            post["category_id"],
-            post["title"],
-            datetime.now(),
-            post["image_url"],
-            post["content"],
-            post["approved"],
-        ),)
+        """,
+            (
+                post["user_id"],
+                post["category_id"],
+                post["title"],
+                datetime.now(),
+                post["image_url"],
+                post["content"],
+                post["approved"],
+            ),
+        )
         post_id = db_cursor.lastrowid
 
         for tag in post["tags"]:
-            db_cursor.execute("""
+            db_cursor.execute(
+                """
             SELECT id FROM tags WHERE id = ?
-            """, (tag,))
+            """,
+                (tag,),
+            )
             tag_id = db_cursor.fetchone()[0]
 
-            db_cursor.execute("""
+            db_cursor.execute(
+                """
         INSERT INTO posttags ('post_id', 'tag_id') 
                               VALUES (?, ?)   
-        """, (post_id, tag_id))
-            
+        """,
+                (post_id, tag_id),
+            )
+
         conn.commit()
 
         return json.dumps({"success": True})
+
 
 def get_user_posts(post_data, user):
     with sqlite3.connect("./db.sqlite3") as conn:
@@ -43,7 +54,8 @@ def get_user_posts(post_data, user):
 
         if post_data:
             if user:
-                db_cursor.execute("""
+                db_cursor.execute(
+                    """
                 SELECT
                     p.id,
                     p.title,
@@ -71,4 +83,3 @@ def get_user_posts(post_data, user):
         serialized_posts = json.dumps(posts)
 
     return serialized_posts
-
